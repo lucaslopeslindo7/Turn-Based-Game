eb2ef85796abaae50f4c0c1a8bad58f1439b9606
lucaslopeslindo7 <lucasrlopes2007@gmail.com>
2026-04-06 17:08:18 -0300

    finished

--- Turn Based Game/Program.cs
+++ Turn Based Game/Program.cs
@@ -1,16 +1,60 @@
 ﻿using System;
 
-bool combatWon = false;
-
-IEntity newHero = new Hero("John", 20, 100);
-IEntity newEnemy = new Enemy("Evil Guy", 10, 50);
+Entity newHero = new Entity("John", 20, 400);
+Entity newEnemy = new Entity("Evil Guy", 20, 400);
 
 int healingCapability = 10;
+int enemyHealingCapability = 10;
+bool enemyHealedLastTurn = false;
 
 while (newHero.Health > 0 && newEnemy.Health > 0)
 {
-    Console.WriteLine("== Player's Turn ==");
+    Console.WriteLine("\n== Player's Turn ==");
     Console.WriteLine("Enter 'attack' to attack, enter 'heal' to heal. Your current healing capability is: {0}",healingCapability);
+    Console.WriteLine("Current Health: {0}/{1}",newHero.Health,newHero.MaxHealth);
+    Console.WriteLine("Enemy Health: {0}/{1}", newEnemy.Health,newEnemy.MaxHealth);
+
+    string input = Console.ReadLine();
+
+    Console.WriteLine("\n");
+
+    if (input == "attack" || (input != "attack" && input != "heal"))
+    {
+        healingCapability += 2;
+        newHero.Attack(newEnemy);
+
+        if (newEnemy.Health == 0)
+        {
+            Console.WriteLine("You win!");
+            break;
+        }
+    }
+
+    if (input == "heal")
+    {
+        newHero.Heal(healingCapability);
+        healingCapability -= 2;
+    }
+
+    Console.WriteLine("\n== Enemy's Turn ==");
+
+    if (newEnemy.Health < newEnemy.MaxHealth / 2 && enemyHealedLastTurn != true)
+    {
+        enemyHealingCapability -= 2;
+        enemyHealedLastTurn = true;
+        newEnemy.Heal(enemyHealingCapability);
+    } else
+    {
+        enemyHealingCapability += 2;
+        enemyHealedLastTurn = false;
+        newEnemy.Attack(newHero);
+
+        if (newHero.Health == 0)
+        {
+            Console.WriteLine("You lose!");
+            break;
+        }
+    }
 
 
 }
@@ -31,7 +75,7 @@ public interface IEntity
 
 }
 
-public class Hero : IEntity
+public class Entity : IEntity
 {
     public int Health { get; set; }
     public string Name { get; set; } = string.Empty;
@@ -42,43 +86,43 @@ public class Hero : IEntity
 
     public void Attack(IEntity enemy)
     {
-        enemy.Health = Math.Clamp(enemy.Health - Damage, 0, enemy.MaxHealth);
+        int dealtDamage = Damage;
+
+        Random randomCritChance = new();
+
+        int randomRoll = randomCritChance.Next(1, 6);
+
+        if (randomRoll == 1)
+        {
+            Console.WriteLine("A critical hit!");
+            dealtDamage *= 2;
+        }
+
+        enemy.Health = Math.Clamp(enemy.Health - dealtDamage, 0, enemy.MaxHealth);
+        Console.WriteLine("{0} Attacks {1} with great precision! {2} damage is dealt!", Name, enemy.Name, dealtDamage);
+
     }
 
     public void Heal(int healAmount)
     {
+        int healingDone = healAmount;
+
+        Random randomCritChance = new();
+        Random randomMultiplier = new();
+
+        int randomRoll = randomCritChance.Next(1, 6);
+
+        if (randomRoll == 1)
+        {
+            Console.WriteLine("A critical heal!");
+            healingDone *= randomMultiplier.Next(3, 7);
+        }
+
         Health = Math.Clamp(Health + healAmount, 0, MaxHealth);
+        Console.WriteLine("{0} takes some time to mend their wounds. Healed {1} health!", Name, healAmount);
     }
 
-    public Hero(string name, int damage, int maxHealth)
-    {
-        Name = name;
-        Damage = damage;
-        MaxHealth = maxHealth;
-        Health = maxHealth;
-    }
-}
-
-public class Enemy : IEntity
-{
-    public int Health { get; set; } = 0;
-    public string Name { get; set; } = string.Empty;
-
-    public int Damage { get; private set; }
-
-    public int MaxHealth { get; private set; }
-
-    public void Attack(IEntity enemy)
-    {
-        enemy.Health = Math.Clamp(enemy.Health - Damage, 0, enemy.MaxHealth);
-    }
-
-    public void Heal(int healAmount)
-    {
-        Health = Math.Clamp(Health + healAmount, 0, MaxHealth);
-    }
-
-    public Enemy(string name, int damage, int maxHealth)
+    public Entity(string name, int damage, int maxHealth)
     {
         Name = name;
         Damage = damage;
